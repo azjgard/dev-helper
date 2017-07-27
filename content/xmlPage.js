44a5f602a7f240b82745099092a53d7c56e5147b
Jordin Gardner <jgardner@uti.edu>
2017-07-27 15:05:53 -0700

    commented out test code

--- content/xmlPage.js
+++ content/xmlPage.js
@@ -1,14 +1,15 @@
-// Button for sending requests to background
-var btn = document.createElement('button');
-btn.style.width = "100px";
-btn.style.height = "100px";
-btn.innerHTML = "Send Message";
-btn.style.position = "fixed";
-btn.style.left = "0";
-btn.style.top = "0";
-btn.id = "sendMessage";
+// THIS IS STRICTLY FOR TESTING
+// // Button for sending requests to background
+// var btn = document.createElement('button');
+// btn.style.width = "100px";
+// btn.style.height = "100px";
+// btn.innerHTML = "Send Message";
+// btn.style.position = "fixed";
+// btn.style.left = "0";
+// btn.style.top = "0";
+// btn.id = "sendMessage";
 
-document.body.appendChild(btn);
-document.getElementById('sendMessage').addEventListener('click', function() {
-  chrome.runtime.sendMessage({message: 'new-html-page'});
-});
+// document.body.appendChild(btn);
+// document.getElementById('sendMessage').addEventListener('click', function() {
+//   chrome.runtime.sendMessage({message: 'new-html-page'});
+// });
