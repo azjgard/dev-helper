a0816dcb9bd2018254d61df470ab1810f648fe89
Jordin Gardner <jgardner@uti.edu>
2017-07-28 13:54:37 -0700

    added ez copy functionality in templating and fixed manifest

--- page/main.js
+++ page/main.js
@@ -2,8 +2,64 @@
 var app = new Vue({
   el: '#container',
   data: {
-    title: "XML Preview",
+    title: "Old Slide Text",
     slides: []
+  },
+  methods: {
+    copyText: function(event) {
+      // Select all of the text
+      document.execCommand('selectAll', false, null);
+      // Copy all of the text
+      document.execCommand('copy');
+      // Reset the selection
+      window.getSelection().empty();
+    },
+
+    // breaking the Vue paradigm so we can copy text :)
+    copyAll: function(event) {
+      // Get the slide element
+      let slide = event.toElement.parentElement;
+
+      // Get the utility dialog element
+      let utilityDialog = slide.getElementsByClassName('utility-dialog')[0];
+
+      // Get the elements that contain the text we want
+      let textContainer = slide.getElementsByClassName('text-nodes')[0];
+      let textNodes     = textContainer.childNodes;
+
+      // All of the text to be copied will be put in this variable
+      let copyString = '';
+
+      // The character used to separate the nodes
+      let separator = ' ';
+
+      // Build the copy string with the text nodes
+      for (var i = 0; i < textNodes.length; i++) {
+	let formattedText = textNodes[i]
+	      .textContent
+	      .replace(/\s{2,}/g, ' ')
+	      .trim();
+	if (formattedText !== '') {
+	  copyString += formattedText;
+	  if (i < textNodes.length - 1) {
+	    copyString += separator;
+	  }
+	}
+      }
+
+      // Put the text in the utility dialog
+      utilityDialog.textContent = copyString;
+      utilityDialog.contentEditable = true;
+
+      // Focus the utility dialog, highlight all text, and copy it
+      utilityDialog.focus();
+      document.execCommand('selectAll', false, null);
+      document.execCommand('copy');
+
+      // Reset the utility dialog
+      utilityDialog.contentEditable = false;
+      utilityDialog.textContent = "";
+    }
   }
 });
 
@@ -18,7 +74,7 @@ chrome.runtime.onMessage.addListener(
       var slideAlreadyExists = false;
 
       // change this however necessary
-      var primaryKey = 'primaryKey';
+      var primaryKey = 'slideId';
 
       // check if the slide sent already exists
       for (var i = 0; i < slides.length; i++) {
@@ -33,7 +89,6 @@ chrome.runtime.onMessage.addListener(
       // add slide if it doesn't already exist
       if (!slideAlreadyExists) {
 	addSlide(data);
-	console.log(app._data.slides);
       }
     }
 });
