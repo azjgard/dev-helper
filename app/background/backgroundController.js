16023f490e9ea3b1e23d81fa7a8e5ebf172b0e84
Mitchell Sotto <msotto@uticorp.com>
2017-08-08 08:21:21 -0700

    added background.js to the git ignore

--- app/background/backgroundController.js
+++ app/background/backgroundController.js
@@ -1,7 +1,7 @@
 const newPage    = require('./scripts/newPage.js');
 const background = require('./scripts/background.js');
 
-const template = require('./slideTemplates.js');
+// const template = require('./slideTemplates.js');
 
 newPage();
 background();
