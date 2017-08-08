004638e0bdbd6f43240846b6a5d3bbb34054e33c
Jordin Gardner <jgardner@uti.edu>
2017-08-08 08:49:46 -0700

    updated templates

--- app/background/slideTemplates.js
+++ app/background/slideTemplates.js
@@ -1,9 +1,5 @@
 let path = require('path');
 
-// these are all going to have to be redone. I was removing all of the CDATA
-// tags so that it becomes easier for us to parse the templates, but then
-// I realized that they will have to be in the final template.. which is why
-// they're present in the first place.
 let assembleComponents = require('./templates/assembleComponents.xml');
 let clickMe            = require('./templates/clickMe.xml');
 let exam               = require('./templates/exam.xml');
